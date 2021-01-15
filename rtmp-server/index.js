f34fe2a62fd568bc1e67aa0aeeae9c670b8d38ed
Devanshi goel <devanshi@gmail.com>
2021-01-15 08:47:28 +0530

    Adding Source Code

---
+++ rtmp-server/index.js
@@ -0,0 +1,18 @@
+const NodeMediaServer = require('node-media-server');
+const HTTP_PORT = process.env.HTTP_PORT || 8000;
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
